5bbe59fb11eb9f391f2795b648f6429df22b865d
Xuan Khanh Dang <dangxuankhanh030598@gmail.com>
2019-10-01 21:58:05 +0700

    ai doc duoc cai nay lam cho

--- app/src/main/java/com/example/exercirefour/MainActivity.java
+++ app/src/main/java/com/example/exercirefour/MainActivity.java
@@ -5,9 +5,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +37,22 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button okBtn = findViewById(R.id.okButton);
+        final EditText moneyRentPrice = findViewById(R.id.moneyRentPrice);
+        final DatePicker dateAndTime = findViewById(R.id.dateAndTime);
+
+        dateAndTime.setCalendarViewShown(false);
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextUtils.isEmpty(moneyRentPrice.getText())) {
+                    moneyRentPrice.setError("You must input money rent price !!!");
+                }
+            }
+        });
+
     }
 
     @Override
@@ -49,4 +76,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
